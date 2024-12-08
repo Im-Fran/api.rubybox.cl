849d933562f823472a21bb4c842eb419dc5fb373
Francisco Solis <30329003+Im-Fran@users.noreply.github.com>
2024-12-08 15:20:44 +0000

    lint: aplicados reparaciones de pint ✨

--- app/Http/Controllers/Admin/BusinessController.php
+++ app/Http/Controllers/Admin/BusinessController.php
@@ -11,9 +11,7 @@ use Illuminate\Http\Request;
 use Spatie\QueryBuilder\QueryBuilder;
 
 class BusinessController extends Controller {
-
-    public function index(Request $request)
-    {
+    public function index(Request $request) {
         return QueryBuilder::for(Business::class)
             ->allowedFilters(['name', 'description'])
             ->allowedSorts(['name', 'created_at', 'updated_at'])
